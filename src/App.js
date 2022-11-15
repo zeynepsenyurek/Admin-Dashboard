4b168ce6ceb2b81c9905560afd4d7ea6bf67e058
Zeynep Senyurek <zeynep.senyurek@outlook.com>
2022-11-15 17:18:37 +0300

    User form added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/login/Login";
 import New from "./pages/new/New";
 import Single from "./pages/single/Single";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { userInputs } from "./components/formSource";
 
 function App() {
   return (
@@ -16,12 +17,12 @@ function App() {
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route path="new" element={<New inputs = {userInputs}/>} />
             </Route>
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route path="new" element={<New  inputs = {productInputs}/>} />
             </Route>
           </Route>
         </Routes>
--- src/pages/new/New.js
+++ src/pages/new/New.js
@@ -1,6 +1,7 @@
 import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import { DriveFolderUploadOutlined } from "@mui/icons-material";
 
 const New = () => {
   return (
@@ -13,9 +14,48 @@ const New = () => {
         </div>
         <div className="bottom">
           <div className="left">
-            <img />
+            <img 
+            src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+            alt=""
+            />
           </div>
-          <div className="right">right</div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file"><DriveFolderUploadOutlined className="icon"/></label>
+                <input type="file" id="file" style={{display:  "none" }}/>
+              </div>
+              <div className="formInput">
+                <label>Username</label>
+                <input type="text" placeholder="john_doe"/>
+              </div>
+              <div className="formInput">
+                <label>Name and surname</label>
+                <input type="text" placeholder="John Doe"/>
+              </div>
+              <div className="formInput">
+                <label>Email</label>
+                <input type="email" placeholder="john_doe@gmail.com"/>
+              </div>
+              <div className="formInput">
+                <label>Phone</label>
+                <input type="email" placeholder="+90 7851 571 44"/>
+              </div>
+              <div className="formInput">
+                <label>Password</label>
+                <input type="password"/>
+              </div>
+              <div className="formInput">
+                <label>Address</label>
+                <input type="text" placeholder="Elton St. 216 NewYork"/>
+              </div>
+              <div className="formInput">
+                <label>Country</label>
+                <input type="text" placeholder="USA"/>
+              </div>
+              <button>Send</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
